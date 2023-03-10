7d9f1db89c34e9090ec5eef6fd04ba2cf00a92af
eugene park <e.park5336@gmail.com>
2023-03-09 19:17:44 -0500

    started filling out models controller and confi

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const session = require('express-session');
+const hndlbrs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const app = express();
+
+// connect rest of ultilites to main server file
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3001;
+}
+// borrowed from activity 17
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      // Stored in milliseconds
+      maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, () => console.log('Now listening'));
+  });
